ad0fee18fbcf7c5da3f582c2ed2c03d991ff14b8
Subhro2797 <subhro271197@gmail.com>
2023-03-21 12:19:33 +0600

    fixed something

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,12 +1,14 @@
 import BackpackGallery from '../BackpackGallery/BackpackGallery';
+import Modal from '../Modal/Modal';
 import ShoeGallery from '../ShoeGallery/ShoeGallery';
+import ShoeModal from '../ShoeModal/ShoeModal';
 
 
 const Gallery = () => {
     return (
         <div>
-            <ShoeGallery></ShoeGallery>
-            <BackpackGallery></BackpackGallery>
+            <ShoeGallery><ShoeModal></ShoeModal></ShoeGallery>
+            <BackpackGallery><Modal></Modal></BackpackGallery>
         </div>
     );
 };
