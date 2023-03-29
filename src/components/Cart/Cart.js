6e7168a3410a53667673a61d87e0159301624af6
Subhro2797 <subhro271197@gmail.com>
2023-03-29 11:02:39 +0600

    Added Breakpoints

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,13 +7,13 @@ const Cart = () => {
         <div className='cart-container container'>
             <h1 className='text-start'>Shopping Cart</h1>
             <div className='row '>
-                <div className='shopping-cart col-9 border border-white border-5 rounded-3' >
+                <div className='shopping-cart col-lg-9 col-md-6 col-sm-7 border border-white border-5 rounded-3 ' >
 
                     <hr />
                     <ChosenProduct></ChosenProduct>
                     <ChosenProduct></ChosenProduct>
                 </div>
-                <div className=' order-summary col-3 border border-white border-5 rounded-3'>
+                <div className=' order-summary col-lg-3 col-md-6 col-sm-5 border border-white border-5 rounded-3 '>
                     <h2> <u>Order Summary</u></h2>
                     <p className='fs-5'><strong>Selected Items: </strong> 6</p>
                     <p className='fs-5'><strong>Total Price: </strong> $1140</p>
